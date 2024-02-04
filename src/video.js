8984a5738bb610474113402ea25467c5c6db4ea3
tokkynsakky <sakiyamatokio@icloud.com>
2024-02-04 17:21:28 +0900

    videoの処理の変更

--- src/video.js
+++ src/video.js
@@ -1,76 +1,115 @@
-import React, { useState, useRef, useCallback } from "react";
-import Webcam from "react-webcam";
-
-const videoConstraints = {
-  width: 1920,
-  height: 1080,
-  facingMode: "user",
-};
+import React from "react";
 
 function Video() {
-  const [isCaptureEnable, setCaptureEnable] = useState(false);
-  const webcamRef = useRef(null);
-  const [url, setUrl] = useState(null);
-  const capture = useCallback(() => {
-    const imageSrc = webcamRef.current.getScreenshot();
-    if (imageSrc) {
-      setUrl(imageSrc);
+  const width = 600;
+  let height = 0;
+
+  let streaming = false;
+
+  let video = null;
+  let canvas = null;
+  let photo = null;
+  let startbutton = null;
+
+  function showViewLiveResultButton() {
+    if (window.self !== window.top) {
+      document.querySelector(".contentarea").remove();
+      const button = document.createElement("button");
+      button.textContent = "view live result of the example code above";
+      document.body.append(button);
+      // eslint-disable-next-line
+      button.addEventListener("click", () => window.open(location.href));
+      return true;
     }
-  }, [webcamRef]);
+    return false;
+  }
+
+  async function startup() {
+    if (showViewLiveResultButton()) {
+      return;
+    }
+    video = document.getElementById("video");
+    canvas = document.getElementById("canvas");
+    photo = document.getElementById("photo");
+    startbutton = document.getElementById("startbutton");
+
+    await navigator.mediaDevices
+      .getUserMedia({ video: true, audio: false })
+      .then((stream) => {
+        video.srcObject = stream;
+        video.play();
+      })
+      .catch((err) => {
+        console.error(`An error occurred: ${err}`);
+      });
+
+    video.addEventListener(
+      "canplay",
+      (ev) => {
+        if (!streaming) {
+          height = video.videoHeight / (video.videoWidth / width);
+
+          if (isNaN(height)) {
+            height = width / (4 / 3);
+          }
+
+          video.setAttribute("width", width);
+          video.setAttribute("height", height);
+          canvas.setAttribute("width", width);
+          canvas.setAttribute("height", height);
+          streaming = true;
+        }
+      },
+      false
+    );
+
+    startbutton.addEventListener(
+      "click",
+      (ev) => {
+        takepicture();
+        ev.preventDefault();
+      },
+      false
+    );
+
+    clearphoto();
+  }
+
+  function clearphoto() {
+    const context = canvas.getContext("2d");
+    context.fillStyle = "#AAA";
+    context.fillRect(0, 0, canvas.width, canvas.height);
+
+    const data = canvas.toDataURL("image/png");
+    photo.setAttribute("src", data);
+  }
+
+  function takepicture() {
+    const context = canvas.getContext("2d");
+    if (width && height) {
+      canvas.width = width;
+      canvas.height = height;
+      context.drawImage(video, 0, 0, width, height);
+
+      const data = canvas.toDataURL("image/png");
+      photo.setAttribute("src", data);
+    } else {
+      clearphoto();
+    }
+  }
+
+  window.addEventListener("load", startup, false);
 
   return (
-    <div className="video-container">
-      <h1 className="Title">漫画風写真撮影アプリ</h1>
-      {isCaptureEnable || (
-        <button
-          className="camera-button"
-          onClick={() => setCaptureEnable(true)}
-        >
-          開始
-        </button>
-      )}
-      {isCaptureEnable && (
-        <div>
-          <div>
-            <button
-              className="camera-button"
-              onClick={() => setCaptureEnable(false)}
-            >
-              終了
-            </button>
-          </div>
-          <div>
-            <Webcam
-              audio={false}
-              width={1920}
-              height={1080}
-              ref={webcamRef}
-              screenshotFormat="image/jpeg"
-              videoConstraints={videoConstraints}
-            />
-          </div>
-          <button className="camera-button" onClick={capture}>
-            キャプチャ
-          </button>
-        </div>
-      )}
-      {url && (
-        <div>
-          <div>
-            <button
-              className="camera-button"
-              onClick={() => {
-                setUrl(null);
-              }}
-            >
-              削除
-            </button>
-          </div>
-          <div>
-            <img src={url} alt="Screenshot" />
-          </div>
-        </div>
-      )}
+    <div className="contentarea">
+      <div className="camera">
+        <video id="video">Video stream not available.</video>
+        <button id="startbutton">Take photo</button>
+      </div>
+      <canvas id="canvas"> </canvas>
+      <div className="output">
+        <img id="photo" alt="The screen capture will appear in this box." />
+      </div>
     </div>
   );
 }
